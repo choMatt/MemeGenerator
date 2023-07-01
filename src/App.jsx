08090c933fc3c8053ddc4ca261ec111d4586faf9
choMatt <mattseneris18@gmail.com>
2023-07-01 16:20:22 +0800

    imported header component and meme component and rendered them both

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import Meme from './components/Meme'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <Header />
+      <main>
+      <Meme />
+      </main>
+    </div>
   )
 }
 
