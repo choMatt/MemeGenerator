c32085c66074dfdb82692abaf98dd2d42f65e938
choMatt <mattseneris18@gmail.com>
2023-07-14 09:59:45 +0800

    changed p tag to h3 and renamed the className to header__sub-title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import TrollFace from "../assets/TrollFace,ong"
+import TrollFace from "../assets/TrollFace.png"
 
 const Header = function(){
     return (
-        <header>
-            <img src={TrollFace}/>
-            <h1>Meme Generator</h1>
-            <p>React Course - Project 3</p>
+        <header className="header">
+            <img src={TrollFace} className="header__image" />
+            <h1 className="header__title">Meme Generator</h1>
+            <h3 className="header__sub-title">React Course - Project 3</h3>
         </header>
+
     )
 }
 
