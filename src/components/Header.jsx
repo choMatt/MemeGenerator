6851601fb5486ce0ccb11a8308741751758f6676
choMatt <mattseneris18@gmail.com>
2023-07-14 09:31:34 +0800

    added header and sub header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+// import TrollFace from "../assets"
 
 const Header = function(){
     return (
-        <div>
-            <p>Hello from Hedaer</p>
-        </div>
+        <header>
+            {/* <img src={}/> */}
+            <h1>Meme Generator</h1>
+            <p>React Course - Project 3</p>
+        </header>
     )
 }
 
