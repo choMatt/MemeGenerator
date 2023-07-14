f0fe54e002f0fa0f4ec39746f7a9e6b6b4c2cca2
choMatt <mattseneris18@gmail.com>
2023-07-14 12:28:25 +0800

    cleaned the indentations of the jsx elements and  added form--flex parent div

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,23 +1,25 @@
-import React from "react"
+import React from "react";
 
-export default function Form(props){
-    return (
-        <div className="form form--meme-generator">
-            <form className="form__form">
-                <input
-                    type="text"
-                    name="topText"
-                    className="form__input form__input--topText"
-                    onChange={(event) => props.handleChange(event)}
-                />
-                <input 
-                    type="text" 
-                    name="bottomText"
-                    className="form__input form__input--bottomText"
-                    onChange={(event) => props.handleChange(event)}
-                />
-                <button type="button" className="form__button">Get new image</button>
-            </form>
-        </div>
-    )
+export default function Form(props) {
+  return (
+    <div className="form--flex">
+        <form className="form form--grid">
+          <input
+            type="text"
+            name="topText"
+            className="form__input form__input--topText"
+            onChange={(event) => props.handleChange(event)}
+          />
+          <input
+            type="text"
+            name="bottomText"
+            className="form__input form__input--bottomText"
+            onChange={(event) => props.handleChange(event)}
+          />
+          <button type="button" className="form__button">
+            Get new image
+          </button>
+        </form>
+    </div>
+  );
 }
