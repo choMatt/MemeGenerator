0afe20002f04807cf3ae2d1832a248c9548cdec8
choMatt <mattseneris18@gmail.com>
2023-07-14 11:57:22 +0800

    added memesData state, handleChange logic, and rendered <Form/>

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import './App.css'
 import Header from './components/Header'
 import Meme from './components/Meme'
+import Form from './components/Form'
 
 function App() {
+
+  const [memesData, setMemesData] = useState({
+    topText: '', 
+    bottomText:  '',
+    memeImage: '',
+  })
+
+  console.log(memesData.topText, memesData.bottomText)
+
+  function handleChange(event){
+    const {value, name} = event.target
+    setMemesData((prevMemeData) => {
+      return {
+        ...prevMemeData,
+        [name]: value
+      }
+    })
+  }
 
   return (
     <div>
       <Header />
       <main>
+      <Form 
+        handleChange={handleChange}
+      />
       <Meme />
       </main>
     </div>
