e77f243a6e3024564d05f10ae4a0c442ea1ad09b
choMatt <mattseneris18@gmail.com>
2023-07-14 11:57:56 +0800

    added BEM methodology on cclassName

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,23 @@
 import React from "react"
 
-export default function Form(){
+export default function Form(props){
     return (
-        <></>
+        <div className="form form--meme-generator">
+            <form className="form__form">
+                <input
+                    type="text"
+                    name="topText"
+                    className="form__input form__input--topText"
+                    onChange={(event) => props.handleChange(event)}
+                />
+                <input 
+                    type="text" 
+                    name="bottomText"
+                    className="form__input form__input--bottomText"
+                    onChange={(event) => props.handleChange(event)}
+                />
+                <button type="button" className="form__button">Get new image</button>
+            </form>
+        </div>
     )
 }
